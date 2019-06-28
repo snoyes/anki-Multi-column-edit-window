3bd8ed6ee92aa1fb73605f3888f47ae722e64e64
Arthur Milchior <Arthur@Milchior.fr>
2019-06-28 04:01:01 +0200

    Merge

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# Multi colum note editor (new)
-[Link to add-on](https://ankiweb.net/shared/info/2064123047)
+# Advanced Note Editor
+## Description
+This Anki add-on merges two add-ons:
+* [Multi-column note editor](https://ankiweb.net/shared/info/3491767031) by HSSM. This add-on does not seems to be debugged anymore
+* [Frozen fields](https://ankiweb.net/shared/info/516643804) by Tiago Barroso and Aristotelis P.
+
+Those add-ons seems to be incompatible, and creating a third add-on seems to be the easier way to have both properties.
+
+![Example](ex.png)
+
+### Multi column note editor
 
 ![Multi-column editor](https://raw.github.com/hssm/anki-addons/master/docs/multicolumn_browser.png)
 
@@ -8,27 +17,30 @@ Use multiple columns in the note editor. You can easily change the number of col
 If you have any fields that take up a lot of space, you can configure them to take up an entire row.
 ![Multi-column editor single row](https://raw.github.com/hssm/anki-addons/master/docs/multicolumn_browser_single_row.png)
 
-## Why a second add-on
+Note that the configuration is saved in the collection. Thus, the configuration should be shared from a computer to another one.
 
-This is a great add-on by HSSM. For a few month, the author didn't
-debug their code. They didn't take pull request into account. Thus I
-update a new version, and I hope to keep this debugged. I'll delete
-this if HSSM comes back and takes correction into account.
+### Frozen fields
+Anki supports '''sticky fields'''. A sticky field is a field whose value is not deleted when you switch to a different note. This can be very useful if you are making many notes in which a field either has the '''same value''' or changes very little.
 
-## Configuration
-Using the add-on manager, you can change the setting of this
-add-on.
+Unfortunaly, marking a field as sticky is quite complex and breaks your workflow. Frozen Fields allows you to conveniently mark a field as sticky (freeze) or not sticky (unfreeze) '''right from the note editor'''. It’s much more convenient than the default way.
+
+To freeze/unfreeze a field, just '''click the adjacent snowflake''' or use the corresponding '''hotkey''' (F9 by default). A blue snowflake means that the field is frozen and a grey snowflake means that the field is unfrozen.
 
 ## Warning
-If you have downloaded Frozen Field add-on and disabled it, the result
-may be different from what you expect.
+If you install this add-on, uninstall the add-on Frozen Fields and Multi column note editor. The result otherwise would be unpredictable.
+
+## TODO:
+More beautiful buttons.
 
 ## Links, licence and credits
 
 Key         |Value
 ------------|-------------------------------------------------------------------
-Copyright   | Arthur Milchior <arthur@milchior.fr>
+Maintener   | Arthur Milchior <arthur@milchior.fr>
 Based on    | Anki code by Damien Elmes <anki@ichi2.net>
+            |[Multi-column note editor](https://ankiweb.net/shared/info/3491767031) by HSSM and
+            |[Frozen fields](https://ankiweb.net/shared/info/516643804) by Tiago Barroso and Aristotelis P.
 License     | GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
-Source in   | https://github.com/Arthur-Milchior/anki-template-card-type
+Source in   | https://github.com/Arthur-Milchior/anki-Multi-column-edit-window.git
 Addon number| [1159566613](https://ankiweb.net/shared/info/1159566613)
+Support me on| [![Ko-fi](https://ko-fi.com/img/Kofi_Logo_Blue.svg)](Ko-fi.com/arthurmilchior) or [![Patreon](http://www.milchior.fr/patreon.png)](https://www.patreon.com/bePatron?u=146206)
--- multiColumnEdit.py
+++ multiColumnEdit.py
@@ -1,114 +1,205 @@
-# -*- coding: utf-8 -*-
-# Version: 2.5
-# See github page to report issues or to contribute:
-# https://github.com/hssm/anki-addons
+from anki.lang import _
+from aqt import editor
+from aqt.editor import *
+from aqt.editor import _html
+from aqt.qt import *
+from aqt.utils import shortcut
+import os
+import json
+from anki.hooks import runHook
 
-from anki.hooks import wrap
-from aqt import *
-from aqt.addcards import AddCards
-from aqt.editor import Editor
-import aqt.editor
-from . import config
+old_init = Editor.__init__
+
+def __init__(self, *args, **kwargs):
+    old_init(self, *args, **kwargs)
+Editor.__init__ = __init__
+
+old_setupTags = Editor.setupTags
+def setupTags(self, *args, **kwargs):
+        g = QGroupBox(self.widget)
+        g.setFlat(True)
+        tabLine = QHBoxLayout()
+        tabLine.setSpacing(12)
+        tabLine.setContentsMargins(6,6,6,6)
+
+        self.setupActualTags(tabLine)
+        self.setupNumberColum(tabLine)
+
+        self.outerLayout.addWidget(g)
+        g.setLayout(tabLine)
+Editor.setupTags = setupTags
+
+def setupActualTags(self, tabLine):
+        import aqt.tagedit
+        # tags
+        tagLabel = QLabel(_("Tags"))
+        tabLine.addWidget(tagLabel)
+        self.tags = aqt.tagedit.TagEdit(self.widget)
+        self.tags.lostFocus.connect(self.saveTags)
+        self.tags.setToolTip(shortcut(_("Jump to tags with Ctrl+Shift+T")))
+        tabLine.addWidget(self.tags)
+Editor.setupActualTags = setupActualTags
+
+def setupNumberColum(self, tabLine):
+        label = QLabel("Columns:", self.widget)
+        tabLine.addWidget(label)
+        self.ccSpin = QSpinBox(self.widget)
+        tabLine.addWidget(self.ccSpin)
+        self.ccSpin.setMinimum(1)
+        self.ccSpin.valueChanged.connect(lambda value: self.onColumnCountChanged(value))
+Editor.setupNumberColum = setupNumberColum
+
+oldOnBridgeCmd = Editor.onBridgeCmd
+def onBridgeCmd(self, cmd):
+    if cmd.startswith("toggleFroze"):
+        model = self.note.model()
+        fieldNumber = cmd.split(":", 1)[1]
+        fieldNumber = int(fieldNumber)
+        fieldObject = model['flds'][fieldNumber]
+        fieldObject["sticky"] = not fieldObject.get("sticky", False)
+        self.mw.col.models.save(model)
+        self.loadNote()
+
+    elif cmd.startswith("toggleLineAlone"):
+        model = self.note.model()
+        fieldNumber = cmd.split(":", 1)[1]
+        fieldNumber = int(fieldNumber)
+        fieldObject = model['flds'][fieldNumber]
+        fieldObject["Line alone"] = not fieldObject.get("Line alone", False)
+        self.mw.col.models.save(model)
+        self.loadNote()
+    else:
+        oldOnBridgeCmd(self, cmd)
+Editor.onBridgeCmd = onBridgeCmd
+
+oldSetNote = Editor.setNote
+def setNote(self, *args, **kwargs):
+    oldSetNote(self, *args, **kwargs)
+    if self.note:
+        self.ccSpin.setValue(self.note.model().get("number of columns", 1))
+Editor.setNote = setNote
+
+def loadNote(self, focusTo=None):
+        """Todo
+
+        focusTo -- Whether focus should be set to some field."""
+        if not self.note:
+            return
+
+        # Triple, for each fields, with (field name, field
+        # content modified so that it's image's url can be
+        # used locally, and whether it is on its own line)
+        data = []
+        for ord, (fld, val) in enumerate(self.note.items()):
+            val = self.mw.col.media.escapeImages(val)
+            field = self.note.model()["flds"][ord]
+            lineAlone = field.get("Line alone", False)
+            sticky = field.get("sticky", False)
+            data.append((fld, val, lineAlone, sticky))
+        self.widget.show()
+        self.updateTags()
+
+        def oncallback(arg):
+            if not self.note:
+                return
+            self.setupForegroundButton()
+            self.checkValid()
+            if focusTo is not None:
+                self.web.setFocus()
+            runHook("loadNote", self)
+
+        self.web.evalWithCallback("setFieldsMC(%s, %d, '%s', '%s'); setFonts(%s); focusField(%s); setNoteId(%s)" % (
+            json.dumps(data),
+            self.note.model().get("number of columns", 1),
+            self.resourceToData(icon_path_frozen),
+            self.resourceToData(icon_path_unfrozen),
+            json.dumps(self.fonts()),
+            json.dumps(focusTo),
+            json.dumps(self.note.id)),
+                                  oncallback)
+Editor.loadNote = loadNote
 
 
-# Flag to enable hack to make Frozen Fields look normal
-ffFix = False
+def onColumnCountChanged(self, count):
+        "Save column count to settings and re-draw with new count."
+        model = self.note.model()
+        model["number of columns"] = count
+        self.mw.col.models.save(model)
+        self.loadNote()
+Editor.onColumnCountChanged = onColumnCountChanged
 
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 js_file = os.path.join(__location__,"js.js")
 user_files = os.path.join(__location__,"user_files")
 css_file = os.path.join(user_files,"css.css")
+icon_path = os.path.join(__location__,"icons")
+icon_path_frozen = os.path.join(icon_path, "frozen.png")
+icon_path_unfrozen = os.path.join(icon_path, "unfrozen.png")
 with open(js_file,"r") as f:
     js= f.read()
 with open(css_file,"r") as f:
     css= f.read()
 
-aqt.editor._html += f"""<style>{css}</style><script>{js}</script>"""
-
-def onColumnCountChanged(self, count):
-    "Save column count to settings and re-draw with new count."
-    config.setConfig("count", count, self.note.mid, self.parentWindow.__class__.__name__)
-    self.loadNote()
-
-def myEditorInit(self, mw, widget, parentWindow, addMode=False):
-    self.ccSpin = QSpinBox(self.widget)
-    b = QPushButton(u"▾")
-    b.clicked.connect(lambda: onConfigClick(self))
-    b.setFixedHeight(self.tags.height())
-    b.setFixedWidth(25)
-    b.setAutoDefault(False)
-    hbox = QHBoxLayout()
-    hbox.setSpacing(0)
-    label = QLabel("Columns:", self.widget)
-    hbox.addWidget(label)
-    hbox.addWidget(self.ccSpin)
-    hbox.addWidget(b)
-
-    self.ccSpin.setMinimum(1)
-    self.ccSpin.setMaximum(config.getConfig("MAX_COLUMNS", 18))
-    self.ccSpin.valueChanged.connect(lambda value: onColumnCountChanged(self, value))
-
-    # We will place the column count editor next to the tags widget.
-    pLayout = self.tags.parentWidget().layout()
-    # Get the indices of the tags widget
-    (rIdx, cIdx, r, c) = pLayout.getItemPosition(pLayout.indexOf(self.tags))
-    # Place ours on the same row, to its right.
-    pLayout.addLayout(hbox, rIdx, cIdx+1)
-
-    # If the user has the Frozen Fields add-on installed, tweak the
-    # layout a bit to make it look right.
-    global ffFix
-    if config.getConfig("Frozen Fields number", "516643804") in mw.addonManager.allAddons() and isinstance(self.parentWindow, AddCards):
-        ffFix = True
-    else:
-        pass
+js = f"""<script>{js}</script>"""
+css = f"""<style>{css}</style>"""
 
 
-def myOnBridgeCmd(self, cmd):
-    """
-    Called from JavaScript to inject some values before it needs
-    them.
-    """
-    if cmd == "mceTrigger":
-        count = config.getConfig("count", 1, self.note.mid, self.parentWindow.__class__.__name__)
-        self.web.eval(f"setColumnCount({count});")
-        self.ccSpin.blockSignals(True)
-        self.ccSpin.setValue(count)
-        self.ccSpin.blockSignals(False)
-        for fld, val in self.note.items():
-            if config.getConfig(fld, False, self.note.mid, self.parentWindow.__class__.__name__):
-                self.web.eval(f"setSingleLine('{fld}');")
-        if ffFix:
-            self.web.eval("setFFFix(true)")
-        self.web.eval("makeColumns2()")
+def setupWeb(self):
+        self.web = EditorWebView(self.widget, self)
+        self.web.title = "editor"
+        self.web.allowDrops = True
+        self.web.onBridgeCmd = self.onBridgeCmd
+        self.outerLayout.addWidget(self.web, 1)
 
+        # List of buttons on top right of editor
+        righttopbtns = list()
+        righttopbtns.append(self._addButton('text_bold', 'bold', _("Bold text (Ctrl+B)"), id='bold'))
+        righttopbtns.append(self._addButton('text_italic', 'italic', _("Italic text (Ctrl+I)"), id='italic'))
+        righttopbtns.append(self._addButton('text_under', 'underline', _("Underline text (Ctrl+U)"), id='underline'))
+        righttopbtns.append(self._addButton('text_super', 'super', _("Superscript (Ctrl++)"), id='superscript'))
+        righttopbtns.append(self._addButton('text_sub', 'sub', _("Subscript (Ctrl+=)"), id='subscript'))
+        righttopbtns.append(self._addButton('text_clear', 'clear', _("Remove formatting (Ctrl+R)")))
+        # The color selection buttons do not use an icon so the HTML must be specified manually
+        tip = _("Set foreground colour (F7)")
+        righttopbtns.append('''<button tabindex=-1 class=linkb title="{}"
+            type="button" onclick="pycmd('colour');return false;">
+            <div id=forecolor style="display:inline-block; background: #000;border-radius: 5px;"
+            class=topbut></div></button>'''.format(tip))
+        tip = _("Change colour (F8)")
+        righttopbtns.append('''<button tabindex=-1 class=linkb title="{}"
+            type="button" onclick="pycmd('changeCol');return false;">
+            <div style="display:inline-block; border-radius: 5px;"
+            class="topbut rainbow"></div></button>'''.format(tip))
+        righttopbtns.append(self._addButton('text_cloze', 'cloze', _("Cloze deletion (Ctrl+Shift+C)")))
+        righttopbtns.append(self._addButton('paperclip', 'attach', _("Attach pictures/audio/video (F3)")))
+        righttopbtns.append(self._addButton('media-record', 'record', _("Record audio (F5)")))
+        righttopbtns.append(self._addButton('more', 'more'))
+        righttopbtns = runFilter("setupEditorButtons", righttopbtns, self)
 
-def onConfigClick(self):
-    m = QMenu(self.mw)
-    def addCheckableAction(menu, isCheck, fld):
-        a = menu.addAction(fld)
-        a.setCheckable(True)
-        a.setChecked(isCheck)
-        a.toggled.connect(lambda b, f=fld: onCheck(self, f))
-
-    # Descriptive title thing
-    a = QAction(u"―Single Row―", m)
-    a.setEnabled(False)
-    m.addAction(a)
-
-    for fld, val in self.note.items():
-        isCheck = config.getConfig(fld, False, mid =  self.note.mid, windowName = self.parentWindow.__class__.__name__)
-        addCheckableAction(m, isCheck, fld)
-
-    m.exec_(QCursor.pos())
-
-
-def onCheck(self, field):
-    current = config.getConfig(field, mid = self.note.mid, windowName = self.parentWindow.__class__.__name__)
-    config.setConfig(field, not current, mid = self.note.mid, windowName = self.parentWindow.__class__.__name__)
-    self.loadNote()
-
-
-Editor.__init__ = wrap(Editor.__init__, myEditorInit)
-Editor.onBridgeCmd = wrap(Editor.onBridgeCmd, myOnBridgeCmd, 'before')
+        # Fields... and Cards... button on top lefts, and
+        lefttopbtns = """
+                <button title='%(fldsTitle)s' onclick="pycmd('fields')">%(flds)s...</button>
+                <button title='%(cardsTitle)s' onclick="pycmd('cards')">%(cards)s...</button>
+        """%dict(flds=_("Fields"), cards=_("Cards"),
+                   fldsTitle=_("Customize Fields"),
+                   cardsTitle=shortcut(_("Customize Card Templates (Ctrl+L)")))
+        topbuts= """
+            <div id="topbutsleft" style="float:left;">
+                %(lefttopbtns)s
+            </div>
+            <div id="topbutsright" style="float:right;">
+                %(rightbts)s
+            </div>
+        """ % dict(lefttopbtns = lefttopbtns, rightbts="".join(righttopbtns))
+        bgcol = self.mw.app.palette().window().color().name()
+        # then load page
+        html = _html % (
+            bgcol, bgcol,
+            topbuts,
+            _("Show Duplicates"))
+        self.web.stdHtml(html,
+                         css=["editor.css"], # only difference, css and js file
+                         js=["jquery.js", "editor.js"],
+                         head=js+css)
+Editor.setupWeb = setupWeb
