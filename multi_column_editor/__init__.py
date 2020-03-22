d76d4a08b03e4e84ea99e32971617c5f98099167
Arthur Milchior <Arthur@Milchior.fr>
2020-03-22 14:29:58 +0100

    Apply pep8 and sort imports

--- multi_column_editor/__init__.py
+++ multi_column_editor/__init__.py
@@ -6,18 +6,21 @@
 from anki.hooks import wrap
 from aqt import *
 from aqt.editor import Editor
-import aqt.editor
 
 # A sensible maximum number of columns we are able to set
 
 
 # Settings key to remember column count
-config= mw.addonManager.getConfig(__name__)
+config = mw.addonManager.getConfig(__name__)
 if config is None:
-    config=dict()
-def getConfig(self,key,defaultValue=None):
+    config = dict()
+
+
+def getConfig(self, key, defaultValue=None):
     transferConfig(self)
-    return config.get(key,defaultValue)
+    return config.get(key, defaultValue)
+
+
 # Flag to enable hack to make Frozen Fields look normal
 ffFix = False
 
@@ -146,30 +149,34 @@ $('#fields').bind('DOMNodeInserted', makeColumns);
 </script>
 """
 
-def getKeyForContext(self,field=None):
+
+def getKeyForContext(self, field=None):
     """Get a key that takes into account the parent window type and
     the note type.
-    
+
     This allows us to have a different key for different contexts,
     since we may want different column counts in the browser vs
     note adder, or for different note types.
     """
     key = str(self.note.mid)
-    if getConfig(self,"same config for each window",False):
-        key=f"{self.parentWindow.__class__.__name__}-{key}"
+    if getConfig(self, "same config for each window", False):
+        key = f"{self.parentWindow.__class__.__name__}-{key}"
     if field is not None:
-        key=f"{key}{field}"
+        key = f"{key}{field}"
     return key
 
-def setConfig(self,key,value):
+
+def setConfig(self, key, value):
     config[key] = value
-    mw.addonManager.writeConfig(__name__,config)
+    mw.addonManager.writeConfig(__name__, config)
     #print(f"Setting config[{key}] to {value}")
     self.loadNote()
 
+
 def onColumnCountChanged(self, count):
     "Save column count to settings and re-draw with new count."
-    setConfig(self,getKeyForContext(self),count)
+    setConfig(self, getKeyForContext(self), count)
+
 
 def myEditorInit(self, mw, widget, parentWindow, addMode=False):
     self.ccSpin = QSpinBox(self.widget)
@@ -184,10 +191,11 @@ def myEditorInit(self, mw, widget, parentWindow, addMode=False):
     hbox.addWidget(label)
     hbox.addWidget(self.ccSpin)
     hbox.addWidget(b)
-    
+
     self.ccSpin.setMinimum(1)
-    self.ccSpin.setMaximum(getConfig(self,"MAX_COLUMNS"))
-    self.ccSpin.valueChanged.connect(lambda value: onColumnCountChanged(self, value))
+    self.ccSpin.setMaximum(getConfig(self, "MAX_COLUMNS"))
+    self.ccSpin.valueChanged.connect(
+        lambda value: onColumnCountChanged(self, value))
 
     # We will place the column count editor next to the tags widget.
     pLayout = self.tags.parentWidget().layout()
@@ -195,7 +203,7 @@ def myEditorInit(self, mw, widget, parentWindow, addMode=False):
     (rIdx, cIdx, r, c) = pLayout.getItemPosition(pLayout.indexOf(self.tags))
     # Place ours on the same row, to its right.
     pLayout.addLayout(hbox, rIdx, cIdx+1)
-        
+
     # If the user has the Frozen Fields add-on installed, tweak the
     # layout a bit to make it look right.
     global ffFix
@@ -212,14 +220,14 @@ def myOnBridgeCmd(self, cmd):
     them.
     """
     if cmd == "mceTrigger":
-        count = getConfig(self,getKeyForContext(self), defaultValue=1)
+        count = getConfig(self, getKeyForContext(self), defaultValue=1)
         self.web.eval(f"setColumnCount({count});")
         self.ccSpin.blockSignals(True)
         self.ccSpin.setValue(count)
         self.ccSpin.blockSignals(False)
         for fld, val in self.note.items():
-            key=getKeyForContext(self,field=fld)
-            if getConfig(self,key, False):
+            key = getKeyForContext(self, field=fld)
+            if getConfig(self, key, False):
                 self.web.eval(f"setSingleLine('{fld}');")
         if ffFix:
             self.web.eval("setFFFix(true)")
@@ -228,26 +236,27 @@ def myOnBridgeCmd(self, cmd):
 
 def onConfigClick(self):
     m = QMenu(self.mw)
+
     def addCheckableAction(menu, key, text):
         a = menu.addAction(text)
         a.setCheckable(True)
-        a.setChecked(getConfig(self,key, False))
+        a.setChecked(getConfig(self, key, False))
         a.toggled.connect(lambda b, k=key: onCheck(self, k))
 
     # Descriptive title thing
     a = QAction(u"―Single Row―", m)
     a.setEnabled(False)
     m.addAction(a)
-    
+
     for fld, val in self.note.items():
-        key = getKeyForContext(self,field=fld)
+        key = getKeyForContext(self, field=fld)
         addCheckableAction(m, key, fld)
 
     m.exec_(QCursor.pos())
 
 
 def onCheck(self, key):
-    setConfig(self,key,not getConfig(self,key))
+    setConfig(self, key, not getConfig(self, key))
 
 
 Editor.__init__ = wrap(Editor.__init__, myEditorInit)
@@ -258,10 +267,12 @@ Editor.onBridgeCmd = wrap(Editor.onBridgeCmd, myOnBridgeCmd, 'before')
 #The code below should be eventually be deleted. It is used only to transfer the old configuration to the new one#
 ###############################################
 CONF_KEY_COLUMN_COUNT = 'multi_column_count'
+
+
 def transferConfig(self):
-  if not config.get("transfer done",False):
-    for key,value in mw.pm.profile.items():
-        if CONF_KEY_COLUMN_COUNT in key:
-            key=key.replace(f"{CONF_KEY_COLUMN_COUNT}-","")
-            setConfig(self,key,value)
-    setConfig(self,"transfer done",True)
+    if not config.get("transfer done", False):
+        for key, value in mw.pm.profile.items():
+            if CONF_KEY_COLUMN_COUNT in key:
+                key = key.replace(f"{CONF_KEY_COLUMN_COUNT}-", "")
+                setConfig(self, key, value)
+        setConfig(self, "transfer done", True)
--- strip_combining_characters/__init__.py
+++ strip_combining_characters/__init__.py
@@ -5,20 +5,21 @@
 
 import unicodedata
 
+from anki.find import Finder
+from anki.hooks import wrap
 from aqt import *
 from aqt.browser import DataModel
 from aqt.forms.browser import Ui_Dialog
-from anki.hooks import wrap
-from anki.find import Finder
 
 CONF_KEY_CHECKED = 'strip_comb_checked'
+
 
 def stripCombining(txt):
     "Return txt with all combining characters removed."
     norm = unicodedata.normalize('NFKD', txt)
     return "".join([c for c in norm if not unicodedata.combining(c)])
 
-    
+
 def mySearch(self, txt, _old):
     """Perform a search using a custom search function if the stripping
     checkbox is checked."""
@@ -47,45 +48,46 @@ def myFindText(self, val, args):
 
     # NOTE: the "?" is user input which is already stripped on search
     return "(n.sfld like ? escape '\\' or "\
-            "stripCombining(n.flds) like ? escape '\\')"
+        "stripCombining(n.flds) like ? escape '\\')"
 
-   
+
 def mySetupUi(self, mw):
     """Add new items to the browser UI to allow toggling the add-on."""
-    
+
     # Create a new SQL function that we can use in our queries.
     mw.col.db._db.create_function("stripCombining", 1, stripCombining)
 
     # Our UI stuff
     self.scToggleButton = QCheckBox(self.widget)
     self.scToggleLabel = QLabel(" Ignore\nAccents")
-    
+
     # Restore checked state
     self.scToggleButton.setCheckState(mw.col.conf.get(CONF_KEY_CHECKED, 0))
 
     # Save state on toggle
     self.scToggleButton.stateChanged.connect(onChecked)
-    
+
     # Add our items to the right of the search box. We do this by moving
     # every widget out of the gridlayout and into a new list. We simply
     # add our stuff in the new list in the right place before moving them
     # back to gridlayout.
     n_items = self.gridLayout.count()
-    items= []
+    items = []
     for i in range(0, n_items):
         item = self.gridLayout.itemAtPosition(0, i).widget()
         items.append(item)
         if item == self.searchEdit:
             items.append(self.scToggleButton)
             items.append(self.scToggleLabel)
-    
+
     for i, item in enumerate(items):
         self.gridLayout.addWidget(item, 0, i, 1, 1)
-        
-    
+
+
 def onChecked(state):
     """Save the checked state in Anki's configuration."""
     mw.col.conf[CONF_KEY_CHECKED] = state
+
 
 Ui_Dialog.setupUi = wrap(Ui_Dialog.setupUi, mySetupUi)
 DataModel.search = wrap(DataModel.search, mySearch, "around")
